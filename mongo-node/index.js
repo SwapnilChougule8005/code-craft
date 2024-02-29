39c08032e93cd568f0cecb4a4453cac13807d4c4
tejas96Dev <144350757+tejas96Dev@users.noreply.github.com>
2024-02-29 21:19:42 +0530

    implement ws

--- mongo-node/index.js
+++ mongo-node/index.js
@@ -1,57 +1,36 @@
-// Installing mongoose -> completed
-// Establish connection with mongo DB server
-// Create Schema/Models along with mongoose configuration
-// Create repository design pattern
-// Creates methods for DB transactions
-// Create service layer to consume repository methods/run some business logic
-// Setup node server
-// Finally create an api to consume data from database or create data into database
-
-// mongoose client lib
-
-
-// singleton design pattern implementation 
-// connect all layers 
-// perform db operation 
-// implement config for project
-// configure eslint and prettier
-// how to host mongo db on cloud
-
-
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 import express from 'express';
+import WebSocket from 'ws';
+import http from 'http';
 import { RootController } from './src/controller';
 import { envConfig } from './src/config';
 
+const PORT = envConfig.getPort();
 const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.use(express.json())
-app.use(RootController)
+wss.on('connection', (ws) => {
+  ws.on('message', (message) => {
+    // Broadcast the message to all connected clients
+    wss.clients.forEach((client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+});
 
-const PORT = envConfig.getPort()
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/src/views/index.html`);
+});
 
-// start server
-app.listen(PORT, ()=>{
-  console.log(`server is running on http://localhost:${PORT}`)
-  
+app.use(express.json());
+app.use(RootController);
+
+server.listen(PORT, () => {
   mongoose.connect(envConfig.getMongoDBUrl())
-  .then(() =>
-    console.log('Connected!'))
-  .catch((err)=>{console.log(error)})
-})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .then(() => console.log('Connected!'))
+    .catch((err) => { console.log(error); });
+  console.log(`server is listing on port ${PORT}`);
+});
